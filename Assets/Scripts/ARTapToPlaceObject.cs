734b786c22e64f51ebe02e8bb7bf9a76206cf8c8
Kretek <szelinski.p@gmail.com>
2020-11-02 22:45:24 +0100

    Hello AR project

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObjectToInstantiate;
+    private GameObject spawnedObject;
+    private ARRaycastManager arRaycastmanager;
+    private Vector2 touchPosition;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        arRaycastmanager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out touchPosition))
+            return;
+
+        if(arRaycastmanager.Raycast(touchPosition, hits, TrackableType.Planes))
+        {
+            var hitPose = hits[0].pose;
+            if(spawnedObject == null)
+            {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
