3ae39491b38e180846d26b0dbca862f6d11e1f7a
Kretek <szelinski.p@gmail.com>
2020-11-12 16:05:22 +0100

    Allow to place multiple cubes, allow to place cubes relative to each other

---
+++ Assets/src/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,35 @@
+﻿using Controls;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using VirtualObjects;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObjectToInstantiate;
+    private readonly Logger logger = new Logger(Debug.unityLogger);
+    private ARRaycastManager arRaycastmanager;
+    private VirtualObjectsManager virtualObjectsManager;
+    private TouchDetector touchDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void Awake()
+    {
+        arRaycastmanager = GetComponent<ARRaycastManager>();
+        virtualObjectsManager = new VirtualObjectsManager(gameObjectToInstantiate);
+        touchDetector = new TouchDetector(virtualObjectsManager, arRaycastmanager, logger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!touchDetector.CheckForTouch())
+            return;
+
+        touchDetector.ResolveLastTouch();
+    }
+}
